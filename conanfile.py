4df3d994c38b1990000e41fec4609de585e237ab
Pat Mancuso <46453397+patmantru@users.noreply.github.com>
2022-01-05 09:21:13 -0500

    Update to consume client-cpp 0.2.7 (#2)

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class clientPythonConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
     def requirements(self):
-        self.requires("opentdf-client/0.2.3@")
+        self.requires("opentdf-client/0.2.7@")
         self.requires("pybind11/2.6.2@")
 
     def build(self):
