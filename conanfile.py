d36b8aa28c590f6b90421a6f4aca2d41b642fa2b
Pat Mancuso <pmancuso@virtru.com>
2022-02-10 14:42:27 -0500

    Update client-cpp version to match

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class clientPythonConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
     def requirements(self):
-        self.requires("opentdf-client/0.2.7@")
+        self.requires("opentdf-client/0.5.1@")
         self.requires("pybind11/2.6.2@")
 
     def build(self):
