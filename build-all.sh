e12da3129d74e994b2d03d92a753de928ba97830
Pat Mancuso <pmancuso@virtru.com>
2022-08-23 10:03:04 -0400

    Assign VBUILD_UNIT_TESTS to avoid attempting them

--- build-all.sh
+++ build-all.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -ex
 rm -rf build
 mkdir build
 cd build
-conan install .. --build=missing
+export VBUILD_UNIT_TESTS="false"
 conan install .. --build=missing
 conan build .. -bf .
 cd ..
