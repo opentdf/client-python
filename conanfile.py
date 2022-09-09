9f0e7f512cdbde5aff9f527bc6a7075eb58ac8c4
Pat Mancuso <pmancuso@virtru.com>
2022-09-09 14:47:37 -0400

    use stock builds

--- conanfile.py
+++ conanfile.py
@@ -9,14 +9,6 @@ class clientPythonConan(ConanFile):
         self.requires("opentdf-client/1.1.5@")
         self.requires("pybind11/2.6.2@")
 
-    # Skip libiconv - not needed
-    def configure(self):
-        self.options["libxml2"].iconv = False
-        self.options["libxml2"].icu = False
-        self.options["boost"].i18n_backend_iconv = "off"
-        self.options["boost"].i18n_backend_icu = False
-        self.options["boost"].without_locale = True
-
     def build(self):
         cmake = CMake(self)
         cmake.configure()
