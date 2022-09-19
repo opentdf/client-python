abf0ad9baa8b133e036bdd79a2a005c1c854ec11
sujan kota <sujankota@gmail.com>
2022-09-19 14:30:32 -0400

    PLAT-2060 - Build whl files for python 3.8,3.9,3.10

--- build-all.sh
+++ build-all.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -ex
+
+# Install opentdf cpp library
 rm -rf build
 mkdir build
 cd build
-export VBUILD_UNIT_TESTS="false"
 conan install .. --build=missing
-conan build .. -bf .
-cd ..
+
+# Build whl
+cd ../src/module
+python3.8 setup.py bdist_wheel 
+python3.9 setup.py bdist_wheel 
+python3.10 setup.py bdist_wheel
