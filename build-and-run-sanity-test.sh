9396b2dc1a3da3ca1702a074c3e9d9b44f0ab10c
Mikhail Ustyantsev <mustyantsev@lohika.com>
2022-09-26 18:13:45 +0300

    build python package

--- build-and-run-sanity-test.sh
+++ build-and-run-sanity-test.sh
@@ -4,8 +4,9 @@ echo "Building python wrapper"
 cd ../.. && ./build-all.sh
 
 echo "Run the sanity test"
-PYV=$(python -c 'import sys; print(".".join(map(str, sys.version_info[0:2])))')
+ls -al
+PYV=$(python -c 'import sys; print("".join(map(str, sys.version_info[0:2])))')
 unzip -l `find . -name \*.whl`
-pip3 install `find . -name \*{PYV}*.whl` --force-reinstall
+pip3 install `find . -name \*${PYV}*.whl` --force-reinstall
 cd src/sample
 python3 ./oidc-auth.py
