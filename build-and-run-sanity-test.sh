b1d8c204ac7b5f74239e3dff1bbbbcb41981e0a3
Mikhail Ustyantsev <mustyantsev@lohika.com>
2022-09-26 17:59:59 +0300

    build python package

--- build-and-run-sanity-test.sh
+++ build-and-run-sanity-test.sh
@@ -4,7 +4,7 @@ echo "Building python wrapper"
 cd ../.. && ./build-all.sh
 
 echo "Run the sanity test"
-PYV=python -c 'import sys; print(".".join(map(str, sys.version_info[0:2])))'
+PYV=$(python -c 'import sys; print(".".join(map(str, sys.version_info[0:2])))')
 unzip -l `find . -name \*.whl`
 pip3 install `find . -name \*{PYV}*.whl` --force-reinstall
 cd src/sample
