ae0573fa12677bc4ad675cd6b0b0f4b8f48f8a3b
Mikhail Ustyantsev <mustyantsev@lohika.com>
2022-10-03 16:32:28 +0300

    build wheels macos

--- build-all.sh
+++ build-all.sh
@@ -6,13 +6,14 @@ export WHEEL_OSX_PLAT_NAME="macosx_10_14_x86_64"
 
 rm -rf $OPENTDF_BUILD
 mkdir $OPENTDF_BUILD
-
+ls
 cd $OPENTDF_BUILD
 conan install .. --build=missing
 conan build .. -bf .
-
+make instal
+ls
+pwd
 cd ../src/python-bindings/pips
-printenv
 python3.10 -m pip install --upgrade pip
 python3.10 -m pip install pybind11 twine
 python3.10 setup.py bdist_wheel --plat-name $WHEEL_OSX_PLAT_NAME
