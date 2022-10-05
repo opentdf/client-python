7053c521b08b26e1e5ac480bd5cbe521eb696faf
Mikhail Ustyantsev <mustyantsev@lohika.com>
2022-10-05 12:32:53 +0300

    win build

--- build-all.bat
+++ build-all.bat
@@ -23,4 +23,7 @@ py -3.9-64 setup.py bdist_wheel
 py -3.10-64 -m pip install wheel pybind11 twine
 py -3.10-64 setup.py bdist_wheel
 
+:fin
+REM return to where we came from
+popd
 exit /b %builderrorlevel%
