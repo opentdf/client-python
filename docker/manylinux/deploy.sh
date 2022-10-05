7615f3e4eb2fff098fff0bf15e56a06c3de2239b
Mikhail Ustyantsev <mustyantsev@lohika.com>
2022-10-05 14:12:46 +0300

    manylinux build

--- docker/manylinux/deploy.sh
+++ docker/manylinux/deploy.sh
@@ -9,7 +9,7 @@ DOCKER_IMAGE=quay.io/pypa/$PLAT
 
 # Note that ALL non-fips tests fail to build on manylinux (and have for some time apparently), so we're turning them off
 docker pull $DOCKER_IMAGE
-docker run --rm -ti \
+docker run --rm \
    -v `pwd`:/io \
    -w /io \
    -e PATH=/root/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
