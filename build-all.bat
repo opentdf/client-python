6dcc57f648ad97eafce364d730554ecf2b594943
Mikhail Ustyantsev <mustyantsev@lohika.com>
2022-10-05 11:47:38 +0300

    build wheels macos and windows and multiple python versions

--- build-all.bat
+++ build-all.bat
@@ -14,6 +14,11 @@ if %builderrorlevel% neq 0 goto fin
 REM build python wheel
 cd %CURRENT_DIR%
 cd %CURRENT_DIR%\src\python-bindings\pips
+py -3.8-64 -m pip install wheel pybind11 twine
+py -3.8-64 setup.py bdist_wheel
+
+py -3.9-64 -m pip install wheel pybind11 twine
+py -3.9-64 setup.py bdist_wheel
 
 py -3.10-64 -m pip install wheel pybind11 twine
 py -3.10-64 setup.py bdist_wheel
