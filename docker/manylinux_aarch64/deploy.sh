7bd5813dc484f7a28de83ae98a949d83ab3b7bc6
Mikhail Ustyantsev <mustyantsev@lohika.com>
2022-10-07 09:46:54 +0300

    manylinux-aarch64 wheels

--- docker/manylinux_aarch64/deploy.sh
+++ docker/manylinux_aarch64/deploy.sh
@@ -9,14 +9,10 @@ DOCKER_IMAGE=quay.io/pypa/$PLAT
 
 # Note that ALL non-fips tests fail to build on manylinux (and have for some time apparently), so we're turning them off
 docker pull $DOCKER_IMAGE
-docker run --platform linux/arm64/v8 --rm \
-   -v "${BUILDKITE_BUILD_CHECKOUT_PATH:-${PWD}}/dist":/io/src/python-bindings/pips/dist \
+docker run --rm \
    -v `pwd`:/io \
    -w /io \
    -e PATH=/root/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
-   -e BUILDKITE_BRANCH=${BUILDKITE_BRANCH} \
-   -e BUILDKITE=${BUILDKITE} \
-   -e BUILDKITE_BUILD_NUMBER=${BUILDKITE_BUILD_NUMBER} \
    -e PLAT=$PLAT \
    -e VIRTRU_RUN_E2E_TESTS=false \
    $DOCKER_IMAGE \
