201cb1c548141210f7f69d6e2f5cf1f89293f50c
sujan kota <sujankota@gmail.com>
2022-10-25 10:26:56 -0400

    Fix the aarch64 build

--- docker/manylinux_aarch64/install_req.sh
+++ docker/manylinux_aarch64/install_req.sh
@@ -2,17 +2,14 @@
 set -e -x
 
 # Install a system package required by our library
-yum install -y centos-release-scl devtoolset-9 rh-python38-python-devel scl-utils wget
+yum install -y centos-release-scl devtoolset-9 rh-python38-python-devel python3-pip scl-utils wget 
 
 source /opt/rh/devtoolset-9/enable
 source /opt/rh/rh-python38/enable
-export PATH=$PATH:/opt/_internal/tools/bin
-export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/usr/local/lib64:/usr/lib64
-strings libstdc++.so.6.0.28 | grep GLIBCXX
 
-pip install --upgrade pip
-pip install --upgrade cmake setuptools ninja scikit-build pybind11
-pip install conan
+pip3 install --upgrade pip
+pip3 install --upgrade setuptools ninja scikit-build pybind11
+pip3 install --user conan
 
 ./build-all.sh
 ./docker/manylinux_aarch64/build_wheels.sh
