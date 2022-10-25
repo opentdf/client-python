777892b06fd59f1a822fb5adeb11942a048ea8cb
Mikhail Ustyantsev <mustyantsev@lohika.com>
2022-10-25 14:48:21 +0300

    glibc not found

--- docker/manylinux_aarch64/install_req.sh
+++ docker/manylinux_aarch64/install_req.sh
@@ -7,8 +7,6 @@ yum install -y centos-release-scl devtoolset-9 rh-python38-python-devel scl-util
 source /opt/rh/devtoolset-9/enable
 source /opt/rh/rh-python38/enable
 export PATH=$PATH:/opt/_internal/tools/bin
-export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/usr/local/lib64:/usr/lib64
-strings libstdc++.so.6.0.28 | grep GLIBCXX
 
 pip install --upgrade pip
 pip install --upgrade cmake setuptools ninja scikit-build pybind11
