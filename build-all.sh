371b0aca4d3f9597013c7f82dd99a5684b93b1d7
sujan kota <sujankota@gmail.com>
2023-09-26 14:35:30 -0400

    Fix the build

--- build-all.sh
+++ build-all.sh
@@ -7,23 +7,62 @@ set -ex
 export OPENTDF_BUILD="build"
 export OPENTDF_DST="opentdf-cpp"
 
+# This naming contains the *MINIMUM* OS version supported, which may not be the one running currently
+if [[ $OSTYPE == "darwin"* ]]; then
+  if [[ `uname -m` == "arm64" ]]; then
+    export WHEEL_OSX_PLAT_NAME="macosx_12_0_arm64"
+  else
+    export WHEEL_OSX_PLAT_NAME="macosx_10_14_x86_64"
+  fi
+fi
+
 rm -rf $OPENTDF_DST
 rm -rf $OPENTDF_BUILD
 mkdir $OPENTDF_BUILD
-mkdir $OPENTDF_DST
-mkdir $OPENTDF_DST/lib
-mkdir $OPENTDF_DST/include
+
 
 cd $OPENTDF_BUILD
 conan --version
 conan install .. --build=missing
+conan build .. --build-folder .
+
+#conan build .. --build-folder .
 
 # We should able to use cibuildwheel on windows and linux
+# cd ../src/python-bindings/pips
+# if [[ $OSTYPE == "darwin"* ]]; then
+#   export CIBW_ARCHS_MACOS="x86_64"
+#   export CIBW_SKIP="cp36-* cp12-*"
+#   python3 -m pip install --upgrade pip
+#   python3 -m pip install cibuildwheel --force
+#   cibuildwheel --platform macos
+# fi
+
+
 cd ../src/python-bindings/pips
 if [[ $OSTYPE == "darwin"* ]]; then
-  export CIBW_ARCHS_MACOS="x86_64"
-  export CIBW_SKIP="cp36-* cp12-*"
-  python3 -m pip install --upgrade pip
-  python3 -m pip install cibuildwheel --force
-  cibuildwheel --platform macos
+  # python3 -m pip install --upgrade pip
+  # python3 -m pip install wheel==$VER_WHEEL pybind11==$VER_PYBIND twine==$VER_TWINE --force
+  # python3 setup.py bdist_wheel --plat-name $WHEEL_OSX_PLAT_NAME
+
+  export PATH="/opt/homebrew/Cellar/python@3.8/3.8.16/bin:$PATH"
+  python3.8 -m pip install --upgrade pip
+  python3.8 -m pip install pybind11 twine
+  python3.8 setup.py bdist_wheel --plat-name $WHEEL_OSX_PLAT_NAME
+
+  export PATH="/opt/homebrew/Cellar/python@3.9/3.9.16/bin:$PATH"
+  python3.9 -m pip install --upgrade pip
+  python3.9 -m pip install pybind11 twine
+  python3.9 setup.py bdist_wheel --plat-name $WHEEL_OSX_PLAT_NAME
+
+  export PATH="//opt/homebrew/Cellar/python@3.10/3.10.10_1/bin:$PATH"
+  python3.10 -m pip install --upgrade pip
+  python3.10 -m pip install pybind11 twine
+  python3.10 setup.py bdist_wheel --plat-name $WHEEL_OSX_PLAT_NAME
+
+  export PATH="/opt/homebrew/Cellar/python@3.11/3.11.2_1/bin:$PATH"
+  python3.11 -m pip install --upgrade pip
+  python3.11 -m pip install pybind11 twine
+  python3.11 setup.py bdist_wheel --plat-name $WHEEL_OSX_PLAT_NAME
 fi
+
---
+++ conanfile.py
@@ -0,0 +1,19 @@
+from conans import ConanFile, CMake
+import os
+
+class VirtruTDF3LibConan(ConanFile):
+    generators = "cmake", "CMakeToolchain"
+    settings = "os", "compiler", "build_type", "arch"
+
+    # manylinux build of boost without libiconv fails because glibc is too old.  Default settings uses prebuilt binaries and works.  
+    # Disable these options until that can be resolved
+    default_options = ("NOTopentdf-client:without_libiconv=True", "NOTopentdf-client:without_zlib=True")
+
+    def requirements(self):
+        self.requires("pybind11/2.10.1@")
+        self.requires("opentdf-client/1.5.3@")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
