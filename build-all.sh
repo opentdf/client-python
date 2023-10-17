6e600ed3c36d1cb9476721abc422d9b629ec3d38
sujan kota <sujankota@gmail.com>
2023-10-17 11:24:34 -0400

    Fix the m1 build

--- build-all.sh
+++ build-all.sh
@@ -8,6 +8,15 @@ export OPENTDF_BUILD="build"
 export OPENTDF_DST="opentdf-cpp"
 ARCH=`uname -m`
 
+# This naming contains the *MINIMUM* OS version supported, which may not be the one running currently
+if [[ $OSTYPE == "darwin"* ]]; then
+  if [[ `uname -m` == "arm64" ]]; then
+    export WHEEL_OSX_PLAT_NAME="macosx_12_0_arm64"
+  else
+    export WHEEL_OSX_PLAT_NAME="macosx_10_14_x86_64"
+  fi
+fi
+
 rm -rf $OPENTDF_DST
 rm -rf $OPENTDF_BUILD
 mkdir $OPENTDF_BUILD
@@ -24,7 +33,7 @@ conan build .. --build-folder .
 # # We should able to use cibuildwheel on windows and linux
 cd ../src/python-bindings/pips
 if [[ $OSTYPE == "darwin"* ]]; then
-  export CIBW_ARCHS_MACOS=ARCH
+  export CIBW_ARCHS_MACOS=$ARCH
   export CIBW_SKIP="cp36-* cp12-*"
   python3 -m pip install --upgrade pip
   python3 -m pip install cibuildwheel --force
